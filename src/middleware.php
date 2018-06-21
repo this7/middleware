4b89a1808d6c57ac1465d7a83331aefa009cac8b
qinuoyun <qinuoyun@qq.com>
2018-06-21 10:38:44 +0800

    no message

---
+++ src/middleware.php
@@ -0,0 +1,21 @@
+<?php
+namespace this7\middleware;
+/**
+ * This7 Frame
+ * @Author: else
+ * @Date:   2018-01-11 14:04:08
+ * @Last Modified by:   qinuoyun
+ * @Last Modified time: 2018-06-14 16:53:16
+ */
+
+class middleware {
+
+    /**
+     * 中央控制器
+     * @param  string $value [description]
+     * @return [type]        [description]
+     */
+    public function centralControl($value = '') {
+        # code...
+    }
+}
